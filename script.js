92cabab3319cf8e6e0e0d382ce193c31e58b4970
NotZADX <81967389+NotZADX@users.noreply.github.com>
2025-09-15 20:07:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('year').textContent = new Date().getFullYear();
+document.getElementById('cta').addEventListener('click', () => {
+  alert('You clicked the button! Edit script.js to change this.');
+});
