c8564edbb800a2c608f31a81eb00535a95dcee9d
NotZADX <81967389+NotZADX@users.noreply.github.com>
2025-09-28 12:50:29 +0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('year').textContent = new Date().getFullYear();
-document.getElementById('cta').addEventListener('click', () => {
-  alert('You clicked the button! Edit script.js to change this.');
-});
