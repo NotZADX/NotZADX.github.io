329ca2a7bcff3b3102da1f47fa4df2555874b767
NotZADX <81967389+NotZADX@users.noreply.github.com>
2026-01-28 13:09:25 +0300

    Delete src directory

--- src/simcore/actions/validateAction.ts
+++
@@ -1,9 +0,0 @@
-import { actionSchema, actionListSchema, type Action } from "./actionSchema.js";
-
-export const validateAction = (action: unknown): action is Action => {
-  return actionSchema.safeParse(action).success;
-};
-
-export const validateActionList = (actions: unknown): actions is Action[] => {
-  return actionListSchema.safeParse(actions).success;
-};
